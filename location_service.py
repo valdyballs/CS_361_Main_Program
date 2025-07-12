967f9cfbfab4facd44dad0f2e6ad6baeece6f981
valdyballs <michaelsva12@gmail.com>
2025-07-12 15:59:53 -0700

    Created function to validate if a city name is valid (greater than two characters and with no invalid characters)

--- location_service.py
+++ location_service.py
@@ -13,3 +13,31 @@ VALID_STATES = {
     'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
     'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
 }
+
+# common city name patterns to validate
+
+CITY_PATTERN = re.compile(r'^[a-zA-Z\s\-\.\']{2,50}$')
+
+def validate_city_name(city):
+    """
+    This function will validate that the city name is valid! City name must
+    not contain any invalid characters (city_pattern) or be shorter than two
+    characters
+    """
+    if not city or len(city.strip()) < 2:
+        return False, "The city name must be at least two characters long"
+    
+    if not CITY_PATTERN.match(city.strip()):
+        return False, "The city name contains invalid characters"
+    
+    return True, None
+
+def validate_state_code(state):
+    """
+    This function will validate that the state code is valid! Must have two
+    letters (example WA not WAA) and must a valid state (in "valid_states)
+
+    """
+
+print(validate_city_name("Seattle"))
+
