034fd55bbe271c22170e0a5789978dcbc0fd5ab0
valdyballs <michaelsva12@gmail.com>
2025-07-12 15:17:50 -0700

    Cleaned up display_welcome function in greeting and prepping for location microservice

--- main_program.py
+++ main_program.py
@@ -14,9 +14,26 @@ class HomeBuyerCLI:
         self.rentcast_service_url = "http://localhost:5003"
 
     def display_welcome(self):
-
         """
         This function will great the user and give them a general guide of the program
+        as well as some of the commands available to them!
         """
-        print("Welcome to the Value Home Finder...ClI ")
-        print("This CLI will guide you throught the tedious process of finding affordable homes in your area!")
+        print("*" * 75)
+        print("Welcome to the Value Home Finder...CLI... \n ")
+        print("This CLI will guide you throught the tedious process of finding affordable homes in your area!\n")
+        print("Here are some commands available to you!")
+        print("->  help - Show this help message")
+        print("->  quit - Exit the program")
+        print("*" * 75)
+
+    def collect_user_location(self):
+        """
+        This function will collect and validate our user location via a microservice.
+        """
+        print("\n ---Location Information ---")
+
+# test functions as we go :)
+if __name__ =="__main__":
+    cli = HomeBuyerCLI()
+    cli.display_welcome()
+    cli.collect_user_location()
