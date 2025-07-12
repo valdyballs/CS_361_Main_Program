90a47dad9ff719a17a5c79cff80d84b6428ce24a
valdyballs <michaelsva12@gmail.com>
2025-07-12 14:58:40 -0700

    Building out main_program by creating welcome service and getting ready for microservices

--- main_program.py
+++ main_program.py
@@ -1,1 +1,22 @@
-print("hello world")
+import requests
+import json
+import sys
+
+class HomeBuyerCLI:
+    def __init__(self):
+        self.user_location = None
+        self.user_budget = None
+        self.property_data = None
+
+        # my ports yo (probably not these but i need tis for the commit)
+        self.location_service_url = "http://localhost:5001"
+        self.budget_service_url = "http://localhost:5002"
+        self.rentcast_service_url = "http://localhost:5003"
+
+    def display_welcome(self):
+
+        """
+        This function will great the user and give them a general guide of the program
+        """
+        print("Welcome to the Value Home Finder...ClI ")
+        print("This CLI will guide you throught the tedious process of finding affordable homes in your area!")
